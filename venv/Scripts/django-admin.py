757c83e7115e84539d64cb1e9525f957fb6d4f5c
Barry Redman <bredman.309@gladesmore.lan>
2019-02-20 11:46:12 +0000

    Basic setup

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bredman.309\PycharmProjects\socialmediaproject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
