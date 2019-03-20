f5bca4504e78db00617ef6c6d15f03f705615552
Okai-Kuwsh <47816668+Okai-Kuwsh@users.noreply.github.com>
2019-03-20 16:41:17 +0000

    Update urls.py

--- connect/urls.py
+++ connect/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('/profile', views.userProfile, name='userProfile'),
+    path('/profile/<int:id>', views.profile, name='profile'),
+    path('/', views.home, name='home')
 ]
